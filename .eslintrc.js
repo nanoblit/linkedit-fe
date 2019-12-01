0e1e7f69fa9d08ed31a70b54558363a631c3d302
nanoblit <jakub.k.maleta@gmail.com>
2019-12-01 14:23:37 +0100

    Add html to eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -20,7 +20,7 @@ module.exports = {
     ecmaVersion: 2018,
     sourceType: "module"
   },
-  plugins: ["react"],
+  plugins: ["react", "html"],
   rules: {},
   globals: {
     process: "readonly"
